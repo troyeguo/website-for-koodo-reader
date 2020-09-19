1e0c4ad3e65c7f3ab5e9a94e027fb860d9344cfa
troyeguo <guo362429@163.com>
2020-09-19 12:06:12 +0800

    fix bug

--- api/update.js
+++ api/update.js
@@ -1,5 +1,5 @@
-const download = require("../../configs/download");
-const logs = require("../../configs/logs");
+const download = require("../configs/download");
+const logs = require("../configs/logs");
 module.exports = (req, res) => {
   res.status(200).send({ download: download, log: logs[0] });
 };
