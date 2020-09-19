50a3882cf5525cf3e4d9dccba6c1bff9502e2561
troyeguo <guo362429@163.com>
2020-09-19 11:34:15 +0800

    fix bug

---
+++ pages/api/update.js
@@ -0,0 +1,5 @@
+const download = require("../../configs/download");
+const logs = require("../../configs/logs");
+module.exports = (req, res) => {
+  res.status(200).send({ download: download, log: logs[0] });
+};
