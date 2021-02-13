8321dd3b325f421b999cc860c07f7417811cc39c
unknown <guo362429@gmail.com>
2021-02-14 00:22:51 +0800

    fix bug

--- pages/download.js
+++ pages/download.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { withTranslation } from "../i18n";
 import Header from "../components/header";
